9c29f423dda43ed76f7bb205402fe286d0afdd79
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-06-03 13:47:18 +0200

    url 1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://nordictenors.com/',
+  siteUrl: process.env.SITE_URL || 'https://nordictenors.no/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
