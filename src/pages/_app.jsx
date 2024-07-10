64a965fe6269e40d5c5e5abba8d9143571385aff
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-07-10 14:12:50 +0300

    use OG url image

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -129,7 +129,10 @@ export default function App({ Component, pageProps }) {
           property="og:description"
           content="Velkommen skal du være til konsert med Nordic Tenors!"
         />
-        <meta property="og:image" content="public/OG_image_2024.webp" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dt3k2apqd/image/upload/v1720609928/Nordic%20Tenors/OG_image_2024_wrshhx.webp"
+        />
         {schema && (
           <script
             type="application/ld+json"
