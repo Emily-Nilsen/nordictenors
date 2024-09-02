1aaf459d146b74d736e11d59b49a05e8b69cbacc
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-09-02 12:40:31 +0300

    update nyhetsbrev link

--- src/components/Nyhetsbrev.jsx
+++ src/components/Nyhetsbrev.jsx
@@ -1,27 +1,25 @@
-import Link from 'next/link'
-
 export function Nyhetsbrev() {
   return (
     <div className="rounded-xl bg-slate-50 dark:bg-white/5">
-      <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:flex lg:items-center lg:justify-between lg:px-8">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-          Ønsker du å motta Nordic Tenors’ nyhetsbrev?
-        </h2>
-        <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
-          {/* <a
-            href="#"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Get started
-          </a> */}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://nordictenors.us17.list-manage.com/subscribe?u=067fa210513aeafa6dfc27206&id=136c1bfdb3"
-            className="text-sm font-semibold leading-6 text-gray-900"
-          >
-            Nyhetsbrev <span aria-hidden="true">→</span>
-          </a>
+      <div className="px-6 pb-16 pt-3 sm:px-6 sm:pb-24 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Meld deg på nyhetsbrevet vårt{' '}
+            <span>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://nordictenors.us17.list-manage.com/subscribe?u=067fa210513aeafa6dfc27206&id=136c1bfdb3"
+                className="prose-none inline text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl"
+              >
+                HER
+              </a>
+            </span>
+            .
+          </h2>
+          <p className="mx-auto mt-6 max-w-lg text-lg leading-8">
+            Da får du tilgang til spennende nyheter og eksklusivt forhåndssalg!
+          </p>
         </div>
       </div>
     </div>
