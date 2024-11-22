c2c5adaa14401b471c27e395b5716c17f55d96bf
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-11-22 09:51:30 +0300

    update desember comp

--- src/components/Desember.jsx
+++ src/components/Desember.jsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { PlusCircleIcon } from '@heroicons/react/24/outline'
-import { GoBackButton } from './GoBackButton'
 
 function TicketIcon(props) {
   return (
