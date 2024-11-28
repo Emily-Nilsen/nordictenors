6a995a12682917717bb0a5b41f91d9ee12e1c486
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-11-28 11:16:59 +0300

    modify layout to filter out concerts older than today

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
-// import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
@@ -12,7 +11,18 @@ import { Prose } from '@/components/Prose'
 import { Search } from '@/components/Search'
 import { ThemeSelector } from '@/components/ThemeSelector'
 import { Footer } from '@/components/Footer'
-// import useTranslation from 'next-translate/useTranslation'
+
+// Filter past concerts based on their isoDate
+function filterNavigation(navigation) {
+  const today = new Date()
+  return navigation.map((section) => ({
+    ...section,
+    links: section.links.filter((link) => {
+      // Retain links without isoDate or if isoDate is in the future
+      return !link.isoDate || new Date(link.isoDate) >= today
+    }),
+  }))
+}
 
 export const navigation = [
   {
@@ -361,13 +371,10 @@ export const navigation = [
 ]
 
 function Header({ navigation }) {
-  let [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false)
 
   useEffect(() => {
-    function onScroll() {
-      setIsScrolled(window.scrollY > 0)
-    }
-    onScroll()
+    const onScroll = () => setIsScrolled(window.scrollY > 0)
     window.addEventListener('scroll', onScroll, { passive: true })
     return () => {
       window.removeEventListener('scroll', onScroll)
@@ -395,7 +402,6 @@ function Header({ navigation }) {
             height={200}
             className="h-7 w-auto lg:hidden"
           />
-          {/* <Logomark className="w-auto h-7 fill-sky-900 dark:fill-white lg:hidden" /> */}
           <Image
             src="/logos/Nordic_Tenors_1_linje_sølv.webp"
             alt="Nordic Tenors logo"
@@ -403,7 +409,6 @@ function Header({ navigation }) {
             height={200}
             className="hidden lg:block"
           />
-          {/* <Logo className="hidden h-2.5 w-auto fill-sky-900 dark:fill-white md:h-3 lg:block" /> */}
         </Link>
       </div>
       <div className="-my-5 mr-6 sm:mr-8 md:mr-0">
@@ -411,124 +416,46 @@ function Header({ navigation }) {
       </div>
       <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
         <ThemeSelector className="relative z-10" />
-        <div
-          // target="_blank"
-          // rel="noopener noreferrer"
-          // href="https://open.spotify.com/artist/2oYkhGSHICR2on9Vm91zV4?si=L9ARZcKlRXuJNMaM52RBRg&nd=1"
-          className="group"
-          // aria-label="Spotify"
-        >
+        <div className="group">
           <p className="cursor-pointer font-display font-semibold text-sky-800 hover:text-red-700 dark:text-gold-500 dark:hover:text-white">
             EN
           </p>
-          {/* <SpotifyIcon className="w-6 h-6 transition-all duration-150 ease-in-out fill-red-800 group-hover:fill-red-900 dark:fill-white dark:group-hover:fill-gold-500" /> */}
         </div>
       </div>
     </header>
   )
 }
 
-function useTableOfContents(tableOfContents) {
-  let [currentSection, setCurrentSection] = useState(tableOfContents[0]?.id)
-
-  let getHeadings = useCallback((tableOfContents) => {
-    return tableOfContents
-      .flatMap((node) => [node.id, ...node.children.map((child) => child.id)])
-      .map((id) => {
-        let el = document.getElementById(id)
-        if (!el) return
-
-        let style = window.getComputedStyle(el)
-        let scrollMt = parseFloat(style.scrollMarginTop)
-
-        let top = window.scrollY + el.getBoundingClientRect().top - scrollMt
-        return { id, top }
-      })
-  }, [])
-
-  useEffect(() => {
-    if (tableOfContents.length === 0) return
-    let headings = getHeadings(tableOfContents)
-    function onScroll() {
-      let top = window.scrollY
-      let current = headings[0].id
-      for (let heading of headings) {
-        if (top >= heading.top) {
-          current = heading.id
-        } else {
-          break
-        }
-      }
-      setCurrentSection(current)
-    }
-    window.addEventListener('scroll', onScroll, { passive: true })
-    onScroll()
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [getHeadings, tableOfContents])
-
-  return currentSection
-}
-
 export function Layout({ children, title, tableOfContents, frontmatter }) {
-  let layoutType = frontmatter?.layout || 'default'
+  const router = useRouter()
+  const isHomePage = router.pathname === '/'
 
-  // Generate JSON-LD schema for each concert page
-
-  let router = useRouter()
-  let isHomePage = router.pathname === '/'
-
-  let allLinks = navigation.flatMap((section) => section.links)
-  let linkIndex = allLinks.findIndex((link) => link.href === router.pathname)
-  let previousPage = allLinks[linkIndex - 1]
-  let nextPage = allLinks[linkIndex + 1]
-  let section = navigation.find((section) =>
-    section.links.find((link) => link.href === router.pathname)
-  )
-  let currentSection = useTableOfContents(tableOfContents)
-
-  function isActive(section) {
-    if (section.id === currentSection) {
-      return true
-    }
-    if (!section.children) {
-      return false
-    }
-    return section.children.findIndex(isActive) > -1
-  }
-
-  // Function to check if the section title includes "Nordic Tenors"
-  const includesNordicTenors = section?.title.includes('Nordic Tenors')
+  // Filter the navigation to exclude past concerts
+  const filteredNavigation = filterNavigation(navigation)
 
   return (
     <>
-      <Header navigation={navigation} />
+      <Header navigation={filteredNavigation} />
 
       {isHomePage && <Hero />}
 
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
         <div className="hidden lg:relative lg:block lg:flex-none">
-          <div className="absolute inset-y-0 right-0 w-[50vw] bg-gray-50 dark:hidden" />
-          <div className="absolute bottom-0 right-0 top-16 hidden h-12 w-px bg-gradient-to-t from-gray-800 dark:block" />
-          <div className="absolute bottom-0 right-0 top-28 hidden w-px bg-gray-800 dark:block" />
           <div className="sticky top-[4.5rem] -ml-0.5 h-[calc(100vh-4.5rem)] w-64 overflow-y-auto overflow-x-hidden py-16 pl-0.5 pr-8 xl:w-72 xl:pr-16">
-            <Navigation navigation={navigation} />
+            <Navigation navigation={filteredNavigation} />
           </div>
         </div>
         <div className="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16">
           <article>
-            {(title || section) && (
+            {(title || frontmatter) && (
               <header className="mb-9 space-y-1">
-                {section && (
+                {frontmatter && (
                   <p
                     className={clsx(
-                      'text-base font-semibold',
-                      'text-sky-800 dark:text-gold-500',
-                      includesNordicTenors && 'font-logo lowercase' // Apply conditional class here
+                      'text-base font-semibold text-sky-800 dark:text-gold-500'
                     )}
                   >
-                    {section.title}
+                    {frontmatter.title}
                   </p>
                 )}
                 {title && (
@@ -540,97 +467,10 @@ export function Layout({ children, title, tableOfContents, frontmatter }) {
             )}
             <Prose>{children}</Prose>
           </article>
-
-          <dl className="mt-12 flex border-t border-gray-200 pt-6 dark:border-gray-800">
-            {previousPage && (
-              <div>
-                <dt className="text-base font-medium text-gray-900 dark:text-white">
-                  Forrige
-                </dt>
-                <dd className="mt-1">
-                  <Link
-                    href={previousPage.href}
-                    className="text-base font-semibold text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
-                  >
-                    <span aria-hidden="true">&larr;</span> {previousPage.title}
-                  </Link>
-                </dd>
-              </div>
-            )}
-            {nextPage && (
-              <div className="ml-auto text-right">
-                <dt className="text-base font-medium text-gray-900 dark:text-white">
-                  Neste
-                </dt>
-                <dd className="mt-1">
-                  <Link
-                    href={nextPage.href}
-                    className="text-base font-semibold text-gray-500 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
-                  >
-                    {nextPage.title} <span aria-hidden="true">&rarr;</span>
-                  </Link>
-                </dd>
-              </div>
-            )}
-          </dl>
-        </div>
-        <div className="hidden xl:sticky xl:top-[4.5rem] xl:-mr-6 xl:block xl:h-[calc(100vh-4.5rem)] xl:flex-none xl:overflow-y-auto xl:py-16 xl:pr-6">
-          <nav aria-labelledby="on-this-page-title" className="w-56">
-            {tableOfContents.length > 0 && (
-              <>
-                <h2
-                  id="on-this-page-title"
-                  className="font-display text-base font-semibold text-gray-900 dark:text-white sm:text-lg"
-                >
-                  På denne siden
-                </h2>
-                <ol role="list" className="mt-4 space-y-3 text-sm">
-                  {tableOfContents.map((section) => (
-                    <li key={section.id}>
-                      <p>
-                        <Link
-                          href={`#${section.id}`}
-                          className={clsx(
-                            isActive(section)
-                              ? 'text-sky-800 dark:text-gold-500'
-                              : 'font-normal text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300'
-                          )}
-                        >
-                          {section.title}
-                        </Link>
-                      </p>
-                      {section.children.length > 0 && (
-                        <ol
-                          role="list"
-                          className="mt-2 space-y-3 pl-5 text-gray-500 dark:text-gray-400"
-                        >
-                          {section.children.map((subSection) => (
-                            <li key={subSection.id}>
-                              <Link
-                                href={`#${subSection.id}`}
-                                className={
-                                  isActive(subSection)
-                                    ? 'text-sky-800 dark:text-gold-500'
-                                    : 'hover:text-gray-600 dark:hover:text-gray-300'
-                                }
-                              >
-                                {subSection.title}
-                              </Link>
-                            </li>
-                          ))}
-                        </ol>
-                      )}
-                    </li>
-                  ))}
-                </ol>
-              </>
-            )}
-          </nav>
         </div>
       </div>
-      <footer>
-        <Footer />
-      </footer>
+
+      <Footer />
     </>
   )
 }
